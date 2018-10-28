74812df85708395d1b758ee84e64233e0e36f269
SeongkyuLee <seongkyu0617@gmail.com>
2018-10-28 18:00:47 +0900

    feat: Add Navigation to App component

--- App.js
+++ App.js
@@ -1,14 +1,91 @@
 import React from 'react';
-import { View } from 'react-native';
+import { 
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from 'react-navigation'
+import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import Deck from './components/Deck'
+import { white, black } from './utils/colors'
+import AddCard from './components/AddCard'
+import Quiz from './components/Quiz'
+import { 
+  setNotification, 
+  getDecks 
+} from './utils/helpers'
 
-import DeckList from './app/components/DeckList'
+const Tabs = createMaterialTopTabNavigator({
+  'Decks': { 
+    screen: DeckList
+  },
+  'New Deck': {
+    screen: NewDeck
+  }
+})
 
-export default class App extends React.Component {
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs
+  },
+  Deck: {
+    screen: Deck,
+    navigationOptions: {
+      headerTintColor: white,
+      headerLeftTintColor: white,
+      title: 'Deck',
+      headerStyle: {
+        backgroundColor: black
+      }
+    }
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      headerTintColor: white,
+      headerLeftTintColor: white,
+      title: `Add Card`,
+      headerStyle: {
+        backgroundColor: black
+      }
+    }
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      headerTintColor: white,
+      headerLeftTintColor: white,
+      title: `Quiz`,
+      headerStyle: {
+        backgroundColor: black
+      }
+    }
+  }
+})
+
+class App extends React.Component {
+  state = {
+    decks: null
+  }
+  componentDidMount() {
+    setNotification()
+    this.refresh()
+  }
+  refresh() {
+    getDecks()
+      .then((decks) => {
+        this.setState({
+          decks: decks
+        })
+      })
+  }
   render() {
     return (
-      <View>
-        <DeckList />
-      </View>
+      <MainNavigator screenProps={{
+        decks: this.state.decks,
+        refreshDeck: () => this.refresh()
+      }}/>
     );
   }
 }
+
+export default App
