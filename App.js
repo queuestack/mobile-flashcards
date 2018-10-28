d32a53ae16fd8a9b27ebe0f22675a3e75e81684a
SeongkyuLee <seongkyu0617@gmail.com>
2018-10-28 18:12:01 +0900

    feat: Add Navigation

--- App.js
+++ App.js
@@ -1,70 +1,10 @@
 import React from 'react';
-import { 
-  createMaterialTopTabNavigator,
-  createStackNavigator
-} from 'react-navigation'
-import DeckList from './components/DeckList'
-import NewDeck from './components/NewDeck'
-import Deck from './components/Deck'
-import { white, black } from './utils/colors'
-import AddCard from './components/AddCard'
-import Quiz from './components/Quiz'
-import { 
-  setNotification, 
-  getDecks 
-} from './utils/helpers'
-
-const Tabs = createMaterialTopTabNavigator({
-  'Decks': { 
-    screen: DeckList
-  },
-  'New Deck': {
-    screen: NewDeck
-  }
-})
-
-const MainNavigator = createStackNavigator({
-  Home: {
-    screen: Tabs
-  },
-  Deck: {
-    screen: Deck,
-    navigationOptions: {
-      headerTintColor: white,
-      headerLeftTintColor: white,
-      title: 'Deck',
-      headerStyle: {
-        backgroundColor: black
-      }
-    }
-  },
-  AddCard: {
-    screen: AddCard,
-    navigationOptions: {
-      headerTintColor: white,
-      headerLeftTintColor: white,
-      title: `Add Card`,
-      headerStyle: {
-        backgroundColor: black
-      }
-    }
-  },
-  Quiz: {
-    screen: Quiz,
-    navigationOptions: {
-      headerTintColor: white,
-      headerLeftTintColor: white,
-      title: `Quiz`,
-      headerStyle: {
-        backgroundColor: black
-      }
-    }
-  }
-})
+import { setNotification, getDecks } from './app/utils/helpers'
+import MainNavigator from './app/components/Navigators'
 
 class App extends React.Component {
   state = {
-    decks: null
+    decks: []
   }
   componentDidMount() {
     setNotification()
---
+++ app/components/Navigators.js
@@ -0,0 +1,58 @@
+import { createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation'
+
+import DeckList from './DeckList'
+import NewDeck from './NewDeck'
+import Deck from './Deck'
+import AddCard from './AddCard'
+import Quiz from './Quiz'
+import { white, black } from '../utils/colors'
+
+const Tabs = createMaterialTopTabNavigator({
+    'Decks': { 
+      screen: DeckList
+    },
+    'New Deck': {
+      screen: NewDeck
+    }
+  })
+  
+const MainNavigator = createStackNavigator({
+    Home: {
+      screen: Tabs
+    },
+    Deck: {
+      screen: Deck,
+      navigationOptions: {
+        headerTintColor: white,
+        headerLeftTintColor: white,
+        title: 'Deck',
+        headerStyle: {
+          backgroundColor: black
+        }
+      }
+    },
+    AddCard: {
+      screen: AddCard,
+      navigationOptions: {
+        headerTintColor: white,
+        headerLeftTintColor: white,
+        title: `Add Card`,
+        headerStyle: {
+          backgroundColor: black
+        }
+      }
+    },
+    Quiz: {
+      screen: Quiz,
+      navigationOptions: {
+        headerTintColor: white,
+        headerLeftTintColor: white,
+        title: `Quiz`,
+        headerStyle: {
+          backgroundColor: black
+        }
+      }
+    }
+  })
+
+  export default MainNavigator
