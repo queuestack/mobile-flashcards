fdb431a0aecc7452562a0cd114b1272200505f3b
SeongkyuLee <seongkyu0617@gmail.com>
2018-10-28 16:12:42 +0900

    feat: Add constants in store

---
+++ app/store/constants/index.js
@@ -0,0 +1,5 @@
+export const ADD_DECK = 'ADD_DECK'
+export const ADD_DECK_TO_STORAGE = 'ADD_DECK_TO_STORAGE'
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK'
+export const GET_DECKS = 'GET_DECKS'
+export const SEND_CARD = 'SEND_CARD'
